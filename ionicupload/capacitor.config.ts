e375c9305accb7369ac757239503e82506b668f0
Ast1va <yagiz.uzunalioglu@gmail.com>
2024-11-16 00:48:01 +0300

    İonic Upload
    İonic uygulaması içine görsel yüklemeyi amaçlayan bir test yazılımıdır.

---
+++ ionicupload/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.yagizuzunalioglu.ionicupload',
+  appName: 'ionicupload',
+  webDir: 'www'
+};
+
+export default config;
